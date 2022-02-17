f2185bb7b4acde0a5ac57a897cac146711642565
Bemnet Dejene <bdejene19@gmail.com>
2022-02-17 17:31:59 -0500

    finished get request to retrieve data from db

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,4 +1,19 @@
 const notes = require('express').Router();
+const notes_db = require('../db/db.json');
+const fs = require('fs');
 
+notes.get('/', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log('error fetching data from db');
+        } else {
+            let parsedData = JSON.parse(data);
+            res.json(parsedData);
+        }
+    });
+})
 
+notes.post('/', (req, res) => {
+    console.log(`${req.method} fired on /api/notes`)
+})
 module.exports = notes;
